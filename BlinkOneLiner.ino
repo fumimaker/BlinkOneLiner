47622c2756888e876eb4fba5101165a881233826
fumi <fumi01200924@gmail.com>
2020-01-07 12:29:17 +0900

    Update BlinkOneLiner.ino

--- BlinkOneLiner.ino
+++ BlinkOneLiner.ino
@@ -1,9 +1,10 @@
-const int LED = 13;
-void setup() {
-  pinMode(LED, OUTPUT);
+const int LED = 13; //D13に接続されている内蔵LEDを使用
+
+void setup(){
+  pinMode(LED, OUTPUT使用 //出力に設定
 }
 
-void loop() {
-  digitalWrite(LED,!digitalRead(LED));
-  delay(1000);
+void loop(){
+  digitalWrite(LED, !digitalRead(LED)); //出力を反転
+  delay(1000);                          //1000ms遅延
 }
