c1a241f447cd6ee36e5a5c58f80d6a05287a17c0
fumi <fumi01200924@gmail.com>
2020-01-07 11:43:35 +0900

    Update BlinkOneLiner.ino

--- BlinkOneLiner.ino
+++ BlinkOneLiner.ino
@@ -1,11 +1,9 @@
 const int LED = 13;
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(LED,!digitalRead(LED));
   delay(1000);
 }
